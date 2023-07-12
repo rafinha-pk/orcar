3c355d101f3e86f58702f2e29e5715dfd351b7e1
Rafael Oliveira <rafinhapk77@gmail.com>
2023-07-12 09:43:43 -0300

    Criação dos colaboradores

---
+++ colaboradores/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Colaboradores, Setores
+
+admin.site.register(Colaboradores)
+admin.site.register(Setores)
--- orcar/settings.py
+++ orcar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'colaboradores',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'orcar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'orcar',
+        'USER': 'rafael',
+        'PASSWORD': 'papa',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BT'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
