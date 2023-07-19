bee8c8aa538d14f18154d9cc8c90056f7bec2539
Rafael Oliveira <rafinhapk77@gmail.com>
2023-07-19 17:37:45 -0300

    CRUD clientes

--- orcamento/views.py
+++ orcamento/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, HttpResponseRedirect)
 from django.http import HttpResponse
 from .models import Clientes
 from .forms import ClienteForm
@@ -15,3 +15,34 @@ def cliente_index(request):
 	context["dataset"] = Clientes.objects.all()
 
 	return render(request, "cliente_index.html", context)
+
+def cliente_detail(request, pk):
+	context = {}
+	context["data"] = Clientes.objects.get(id = pk)
+
+	return render(request, "cliente_detail.html", context)
+
+def cliente_atualiza(request, pk):
+	context = {}
+	obj = get_object_or_404(Clientes, id = pk)
+
+	form = ClienteForm(request.POST or None, instance = obj)
+
+	if form.is_valid():
+		form.save()
+		return HttpResponseRedirect("/cliente/" + str(pk))
+	context["form"] = form
+
+	return render(request, "cliente_atualiza.html", context)
+
+def cliente_delete(request, pk):
+	context = {}
+	obj = get_object_or_404(Clientes, id = pk)
+
+	if request.method == "POST":
+		obj.delete()
+		return HttpResponseRedirect("/cliente/")
+	else:
+		context["data"] = Clientes.objects.get(id = pk)
+
+	return render(request, "cliente_delete.html", context)
