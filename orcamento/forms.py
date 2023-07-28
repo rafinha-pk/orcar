a90a7e265fdf37ed1cb57d5e31cf78f5d306da74
Rafael Oliveira <rafinhapk77@gmail.com>
2023-07-28 11:44:59 -0300

    CRUD do Clinete quase pronto (falta exclusão ainda)

--- orcamento/forms.py
+++ orcamento/forms.py
@@ -5,41 +5,19 @@ from django import forms
 from .models import Clientes
 
 class ClienteForm(forms.ModelForm):
-	class Meta:
-		model = Clientes
-		#fields = [
-		#	"nome",
-		#	"telefone",
-		#	"telefone2",
-		#	"contato",
-		#	"email",
-		#	"email2",
-		#	"obs",
-		#]
-		fields = "__all__"
-		widgets = {
-			'data_ultimo': forms.DateInput(
-				attrs={'type':'date'}
-				), 
-			'data_criacao': forms.DateInput(
-				attrs={'type': 'date'}
-				)
-		}
-	def __init__(self, *args, **kwargs):
-		super(ClienteForm, self).__init__(*args, **kwargs)
-		#self.fields['criador'].disabled = True
-		#self.fields['data_criacao'].disabled = True
-		#self.fields['data_ultimo'].disabled = True
-		#self.fields['data_ultimo'].
-	"""nome = forms.CharField(max_length=250)
-	telefone = forms.CharField(max_length=20, required=False)
-	telefone2 = forms.CharField(max_length=20, required=False)
-	contato = forms.CharField(max_length=250, required=False)
-	email = forms.EmailField(max_length=250, required=False)
-	email2 = forms.EmailField(max_length=250, required=False)
-	obs = forms.CharField(widget=forms.Textarea, required=False)
+    class Meta:
+        model = Clientes
+        fields = "__all__"
+        widgets = {
+            'data_ultimo': forms.DateInput(
+                attrs={'type':'date'}
+                ), 
+            'data_criacao': forms.DateInput(
+                attrs={'type': 'date'}
+                )
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-
-	criador = forms.IntegerField(widget=forms.HiddenInput(), required=False)
-	data_criacao = forms.IntegerField(widget=forms.HiddenInput(), required=False)
-	data_ultimo = forms.IntegerField(widget=forms.HiddenInput(), initial=date.today())"""
+        for field in self.fields.values():
+            field.widget.attrs['class']=  'form-control'
--- orcamento/views.py
+++ orcamento/views.py
@@ -8,65 +8,65 @@ from .forms import ClienteForm
 
 
 def orcamento_index(request):
-	html = "Time is"
+    html = "Time is"
 
-	return HttpResponse(html)
+    return HttpResponse(html)
 
 def cliente_index(request):
-	context = {}
-	if request.POST:
-		context["dataset"] = Clientes.objects.all().filter(nome__icontains=request.POST.get('busca')).order_by('-pk')
-	else:
-		context["dataset"] = Clientes.objects.all().order_by('-pk')
-		
-	return render(request, "cliente_index.html", context)
+    context = {}
+    if request.POST:
+        context["dataset"] = Clientes.objects.all().filter(nome__icontains=request.POST.get('busca')).order_by('-pk')
+    else:
+        context["dataset"] = Clientes.objects.all().order_by('-pk')
+        
+    return render(request, "cliente_index.html", context)
 
 def cliente_detail(request, pk):
-	context = {}
-	context["data"] = Clientes.objects.get(id = pk)
+    context = {}
+    context["data"] = Clientes.objects.get(id = pk)
 
-	return render(request, "cliente_detail.html", context)
+    return render(request, "cliente_detail.html", context)
 
 def cliente_cadastra(request):
-	context = {}
-	HOJE = date.today()
-	
-	if request.POST:
-		form = ClienteForm(request.POST or None)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect('/cliente')
-	else:
-		form = ClienteForm(initial={'criador':request.user, 'data_criacao':HOJE, 'data_ultimo': HOJE})
+    context = {}
+    HOJE = date.today()
+    
+    if request.POST:
+        form = ClienteForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/cliente')
+    else:
+        form = ClienteForm(initial={'criador':request.user, 'data_criacao':HOJE, 'data_ultimo': HOJE})
 
-	context["form"] = form
+    context["form"] = form
 
-	return render(request, "cliente_cadastra.html", context)
+    return render(request, "cliente_cadastra.html", context)
 
 def cliente_atualiza(request, pk):
-	context = {}
-	obj = get_object_or_404(Clientes, id=pk)
-	form = ClienteForm(request.POST or None, instance = obj)
-	if request.POST:
-		if form.is_valid():
-			#criador = models.User(pk=form.instance.criador)
-			#criador = get_object_or_404(User, id=str(obj.criador))
-			HOJE = date.today()
-			form.instance.data_ultimo = HOJE 
-			form.save()
-			return HttpResponseRedirect("/cliente/" + str(pk))
-	context["form"] = form
+    context = {}
+    obj = get_object_or_404(Clientes, id=pk)
+    form = ClienteForm(request.POST or None, instance = obj)
+    if request.POST:
+        if form.is_valid():
+            #criador = models.User(pk=form.instance.criador)
+            #criador = get_object_or_404(User, id=str(obj.criador))
+            HOJE = date.today()
+            form.instance.data_ultimo = HOJE 
+            form.save()
+            return HttpResponseRedirect("/cliente/" + str(pk))
+    context["form"] = form
 
-	return render(request, "cliente_atualiza.html", context)
+    return render(request, "cliente_atualiza.html", context)
 
 def cliente_delete(request, pk):
-	context = {}
-	obj = get_object_or_404(Clientes, id = pk)
+    context = {}
+    obj = get_object_or_404(Clientes, id = pk)
 
-	if request.method == "POST":
-		obj.delete()
-		return HttpResponseRedirect("/cliente/")
-	else:
-		context["data"] = Clientes.objects.get(id = pk)
+    if request.method == "POST":
+        obj.delete()
+        return HttpResponseRedirect("/cliente/")
+    else:
+        context["data"] = Clientes.objects.get(id = pk)
 
-	return render(request, "cliente_delete.html", context)
+    return render(request, "cliente_delete.html", context)
