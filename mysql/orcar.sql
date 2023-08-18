39805f5e49f395f34d356203296e6bf284b3dc07
Rafael Oliveira <rafinhapk77@gmail.com>
2023-08-18 17:31:50 -0300

    0.9 Beta

---
+++ mysql/orcar.sql
@@ -0,0 +1,14 @@
+INSERT INTO `orcamento_formapagamento` (`id`, `nome`) VALUES
+(1, 'Dinheiro');
+
+--
+-- Extraindo dados da tabela `orcamento_statusorcamento`
+--
+
+INSERT INTO `orcamento_statusorcamento` (`id`, `nome`) VALUES
+(1, 'Aberto'),
+(2, 'Enviado'),
+(3, 'Atrasado'),
+(4, 'Vendido'),
+(5, 'Cancelado');
+COMMIT;
